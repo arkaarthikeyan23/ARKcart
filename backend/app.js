c1c8ca7ea9f74940139e84d47dadb2c02886b96d
A R Kaarthikeyan <89853712+arkaarthikeyan23@users.noreply.github.com>
2023-07-12 14:48:59 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ dotenv.config({path:path.join(__dirname,"config/config.env")})
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname,'uploads')))
+app.use('uploads',express.static(path.join(__dirname,'uploads')))
 
 
 const products = require('./routes/product')
